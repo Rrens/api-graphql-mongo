7b18e2b3b0c025eab71496fb146e0b17763b5e91
RendyYusuf <yusufrendi313@gmail.com>
2022-10-12 15:27:31 +0700

    manage routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link, NavLink } from "react-router-dom";
+
+import Form from "./components/Form/index";
+import List from "./components/List/index";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/books/:id/edit" element={<Form />} />
+        <Route path="/books/new" element={<Form />} />
+        <Route path="/books/" element={<List />} />
+      </Routes>
+      <NavLink to="/books/new">Form</NavLink>
+      <NavLink to="/books/">List</NavLink>
+      <List />
+      <Form />
     </div>
   );
 }
